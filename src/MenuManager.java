eb1f982790f10f18ca04a4884d6b42ceddd91b3b
shh28 <shh28@192.168.1.133>
2020-03-24 14:23:10 +0900

    creat a menu

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("*** ClothesAndAcc Management System Menu ***");
+		System.out.println(" 1. View ClothesAndAcc Type ");
+		System.out.println(" 2. View ClothesAndAcc Location ");
+		System.out.println(" 3. Edit TypeAndLocation Of ClothesAndAcc ");
+		System.out.println(" 4. Exit");
+		System.out.println("Select one number between 1 - 4:");
+        	}
+
+}
