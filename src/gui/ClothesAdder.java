527a9f5bfa073e778f6b0f95d392bf65df1725a2
shh28 <shh28@LAPTOP-VMP16QL6>
2020-05-31 21:11:01 +0900

    Creat a GUI

---
+++ src/gui/ClothesAdder.java
@@ -0,0 +1,65 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class ClothesAdder extends JFrame {
+
+	public ClothesAdder() {
+		JPanel panel = new JPanel(new SpringLayout());
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelNumber = new JLabel("number: ", JLabel.TRAILING);
+		JTextField fieldNumber = new JTextField(10);
+		labelNumber.setLabelFor(fieldNumber);
+		panel.add(labelNumber);
+		panel.add(fieldNumber);
+		
+		JLabel labelType = new JLabel("Type: ", JLabel.TRAILING);
+		JTextField fieldType = new JTextField(10);
+		labelType.setLabelFor(fieldType);
+		panel.add(labelType);
+		panel.add(fieldType);
+
+		JLabel labelBrande = new JLabel("Brande: ", JLabel.TRAILING);
+		JTextField fieldBrande = new JTextField(10);
+		labelBrande.setLabelFor(fieldBrande);
+		panel.add(labelBrande);
+		panel.add(fieldBrande);
+		
+		JLabel labelColor = new JLabel("Color: ", JLabel.TRAILING);
+		JTextField fieldColor = new JTextField(10);
+		labelColor.setLabelFor(fieldColor);
+		panel.add(labelColor);
+		panel.add(fieldColor);
+		
+		JLabel labelSize = new JLabel("Size: ", JLabel.TRAILING);
+		JTextField fieldSize = new JTextField(10);
+		labelSize.setLabelFor(fieldSize);
+		panel.add(labelSize);
+		panel.add(fieldSize);
+		
+		JLabel labelLocation = new JLabel("Location: ", JLabel.TRAILING);
+		JTextField fieldLocation = new JTextField(10);
+		labelLocation.setLabelFor(fieldLocation);
+		panel.add(labelLocation);
+		panel.add(fieldLocation);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancel"));
+		
+		SpringUtilities.makeCompactGrid(panel, 7, 2, 6, 6, 6, 6);
+		
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setContentPane(panel);
+		this.setVisible(true);
+		
+	}
+
+}
---
+++ src/gui/GuiTest.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+		//MenuSelection menuselection = new MenuSelection();
+		ClothesAdder clothesadder = new ClothesAdder();
+		ClothesViewer clothesViewer = new ClothesViewer();
+
+	}
+
+}
